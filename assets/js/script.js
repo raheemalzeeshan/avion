060caabd48f3f1d79d7ece70704af27332eebd9b
raheemalzeeshan <raheemalzeeshan@gmail.com>
2023-11-16 15:19:57 +0530

    top nav v1.2

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+// Open Menu
+let menuBar_icon = document.querySelector('.menu-bar');
+let navMiddle = document.querySelector("nav .nav-middle");
+let navRight = document.querySelector("nav .nav-right");
+menuBar_icon.addEventListener("click", () => {
+   
+    menuBar_icon.classList.toggle("menu-bar-open")
+})
